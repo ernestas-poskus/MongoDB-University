ee9544476178ee17aff1db9f95b86487c6dad66e
unknown <ernestas.poskus@gmail.com>
2014-02-26 19:54:17 +0200

    Exam completed

---
+++ M101JS/7/exam/2.js
@@ -0,0 +1,1 @@
+db.messages.aggregate([{$unwind: "$headers.To"},{$project: {"_id": 1,"headers.From": 1,"headers.To": 1}}, {$group: {_id: { id: "$_id", from: "$headers.From", to: "$headers.To" },count: { $sum: 1 }}}, {$group: {_id: { from: "$_id.from", to: "$_id.to" },count: { $sum: 1 }}}, {$sort: {count: -1}}, {$limit: 5}])
---
+++ M101JS/7/exam/Final7/count_cats.js
@@ -0,0 +1,1 @@
+db.images.find({tags:'kittens'}).count()
