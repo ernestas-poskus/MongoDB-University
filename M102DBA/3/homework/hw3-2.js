e51cc688b7eb0d2024e14c3ebe94422fa4c2e76a
Ernestas Poskus <ernestas.poskus@gmail.com>
2014-05-26 22:31:08 +0300

    M102DBA week 3 completed

--- M102DBA/3/homework/hw3-2.js
+++ M102DBA/3/homework/hw3-2.js
@@ -1,4 +1,1 @@
-db.zips.aggregate([{ $project: { _id: { $substr: ["$_id", 0, 1] } } },{ $group: { _id: "$_id", n: { $sum: 1 } } }])
-db.zips.aggregate([{ $project: { _id: { $substr: ["$city", 0, 1] } } }, { $group: { _id: "$_id", n: { $sum: 1 }}}])
-db.zips.remove({ city: /^[0-9]/})
-db.zips.count()
+// Answer: 12
---
+++ M102DBA/3/homework/hw3-3.js
@@ -0,0 +1,2 @@
+db.runCommand ( { compact: 'sensor_readings', paddingFactor: 1 } )
+// Answer: 21
